87318a345056f171fe4f9d1c17792feab9260f7f
Steven Chang <stevenchang5000@gmail.com>
2022-07-12 13:14:32 +1000

    #register_tokens - refactor and store RegisteredTokens (#22)

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,15 @@
-use cosmwasm_std::HumanAddr;
+use crate::constants::PREFIX_REGISTERED_TOKENS;
+use cosmwasm_std::{CanonicalAddr, HumanAddr, StdResult, Storage, Uint128};
+use cosmwasm_storage::{PrefixedStorage, ReadonlyPrefixedStorage};
 use schemars::JsonSchema;
+use secret_toolkit::storage::{TypedStore, TypedStoreMut};
 use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Config {
+    pub admin: HumanAddr,
+    pub butt: SecretContract,
+}
 
 #[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone, JsonSchema)]
 pub struct SecretContract {
@@ -8,8 +17,31 @@ pub struct SecretContract {
     pub contract_hash: String,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct Config {
-    pub admin: HumanAddr,
-    pub butt: SecretContract,
+#[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone, JsonSchema)]
+pub struct RegisteredToken {
+    pub address: HumanAddr,
+    pub contract_hash: String,
+    pub sum_balance: Uint128,
+}
+
+// === RegisteredTokens Storage ===
+pub fn read_registered_token<S: Storage>(
+    store: &S,
+    token_address: &CanonicalAddr,
+) -> Option<RegisteredToken> {
+    let registered_tokens_store = ReadonlyPrefixedStorage::new(PREFIX_REGISTERED_TOKENS, store);
+    let registered_tokens_store = TypedStore::attach(&registered_tokens_store);
+    registered_tokens_store
+        .may_load(token_address.as_slice())
+        .unwrap()
+}
+
+pub fn write_registered_token<S: Storage>(
+    store: &mut S,
+    token_address: &CanonicalAddr,
+    registered_token: RegisteredToken,
+) -> StdResult<()> {
+    let mut registered_tokens_store = PrefixedStorage::new(PREFIX_REGISTERED_TOKENS, store);
+    let mut registered_tokens_store = TypedStoreMut::attach(&mut registered_tokens_store);
+    registered_tokens_store.store(token_address.as_slice(), &registered_token)
 }
