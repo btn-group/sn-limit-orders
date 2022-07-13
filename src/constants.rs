53d7eea82fbf18077c7d66c2a52b2084c7c1b5a0
Steven Chang <stevenchang5000@gmail.com>
2022-07-13 11:10:17 +1000

    #query_balance_of_token (#30)

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,7 @@
 pub const BLOCK_SIZE: usize = 256;
 pub const CONFIG_KEY: &[u8] = b"config";
+pub const MOCK_AMOUNT: u128 = 1_000_000_000_000;
+pub const MOCK_BUTT_ADDRESS: &str = "mock-butt-address";
+pub const MOCK_TOKEN_ADDRESS: &str = "mock-token-address";
 pub const PREFIX_ORDERS: &[u8] = b"orders";
 pub const PREFIX_REGISTERED_TOKENS: &[u8] = b"registered_tokens";
