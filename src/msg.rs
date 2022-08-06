e638342c96b6a973cce54bcd8cfe01a1e5659a1f
Steven Chang <stevenchang5000@gmail.com>
2022-08-07 00:17:10 +1000

    set_execution_fee_for_order

--- src/msg.rs
+++ src/msg.rs
@@ -74,6 +74,9 @@ pub enum QueryMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ReceiveMsg {
+    SetExecutionFeeForOrder {
+        position: Option<u32>,
+    },
     CancelOrder {
         position: u32,
     },
