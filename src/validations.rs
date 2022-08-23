579e3f9eae5d39dfef8e3b636a38df1d36de2c57
Steven Chang <stevenchang5000@gmail.com>
2022-08-23 13:15:25 +1000

    create and use validate_uint128

--- src/validations.rs
+++ src/validations.rs
@@ -1,8 +1,16 @@
-use cosmwasm_std::{HumanAddr, StdError, StdResult};
+use cosmwasm_std::{HumanAddr, StdError, StdResult, Uint128};
 
 pub fn authorize(expected: HumanAddr, received: HumanAddr) -> StdResult<()> {
     if expected != received {
         return Err(StdError::Unauthorized { backtrace: None });
+    }
+
+    Ok(())
+}
+
+pub fn validate_uint128(expected: Uint128, received: Uint128, message: &str) -> StdResult<()> {
+    if expected != received {
+        return Err(StdError::generic_err(message));
     }
 
     Ok(())
